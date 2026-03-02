6e04fc54c64d36ec43f7a2fd0c96b90d2a64e9ef
mbpoornesh <mbpoornesh@gmail.com>
2026-03-02 09:04:58 +0530

    Create UC2.java

---
+++ UC2.java
@@ -0,0 +1,38 @@
+class UC2{
+    public static void main(String[] args){
+        System.out.println("\n");
+        System.out.println("   *   *   *    ");
+        System.out.println("   *       * ");
+        System.out.println("   *       * ");
+        System.out.println("   *   *   *   ");
+
+        System.out.println("\n");
+
+        System.out.println("   *   *   *    ");
+        System.out.println("   *       * ");
+        System.out.println("   *       * ");
+        System.out.println("   *   *   *   ");
+
+        System.out.println("\n");
+        System.out.println("   *    *     ");
+        System.out.println("   *        * ");
+        System.out.println("   *        * ");
+        System.out.println("   *    *     ");
+
+        System.out.println("\n");
+        System.out.println("   *    *     ");
+        System.out.println("   *        * ");
+        System.out.println("   *        * ");
+        System.out.println("   *    *     ");
+        System.out.println("   *");
+        System.out.println("   *");
+        System.out.println("   *");
+
+
+
+
+        System.out.println("banner app complete"        );
+        
+    
+    }
+}
